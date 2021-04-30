ce8c644a42d0d8409786f4193dd65a9dbbc2c50b
Erik Behrends <behrends@users.noreply.github.com>
2021-04-30 13:23:44 +0200

    AddLecture mit Chakra gestyled

--- components/AddLecture.jsx
+++ components/AddLecture.jsx
@@ -1,21 +1,35 @@
+import {
+  Box,
+  Button,
+  Checkbox,
+  Input,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+} from '@chakra-ui/react';
+
 export default function AddLecture() {
   return (
-    <div>
-      <div>
-        <label>Titel der Vorlesung</label>
-        <input type="text" />
-      </div>
-      <div>
-        <label>Anzahl der Stunden</label>
-        <input type="text" />
-      </div>
-      <div>
-        <label>Klausur einplanen?</label>
-        <input type="checkbox" />
-        <input type="text" />
-        <label>Minuten</label>
-      </div>
-      <button>Vorlesung einplanen</button>
-    </div>
+    <Box maxW="sm" borderWidth="1px" borderRadius="lg" p={3}>
+      <Input placeholder="Titel der Vorlesung" mb={2} />
+      <Input placeholder="Anzahl der Stunden" mb={4} />
+      <Checkbox>Klausur einplanen?</Checkbox>
+      <NumberInput
+        step={15}
+        defaultValue={30}
+        min={30}
+        max={120}
+        mb={6}
+      >
+        <NumberInputField />
+        <NumberInputStepper>
+          <NumberIncrementStepper />
+          <NumberDecrementStepper />
+        </NumberInputStepper>
+      </NumberInput>
+      <Button colorScheme="teal">Vorlesung erstellen</Button>
+    </Box>
   );
 }
