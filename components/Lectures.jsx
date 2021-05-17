6c6e857ebfa23ae6ba8cc919c94c74e311d31bda
Erik Behrends <behrends@users.noreply.github.com>
2021-05-17 16:52:52 +0200

    Liste der Vorlesungen wird angezeigt

--- components/Lectures.jsx
+++ components/Lectures.jsx
@@ -1,3 +1,25 @@
+import {useEffect, useState} from 'react';
+import { supabase } from '../lib/initSupabase'
+
 export default function Lectures() {
-  return <h1>LECTURES</h1>;
+  const [lectures, setLectures] = useState([]);
+
+  useEffect(() => {
+    fetchLectures()
+  }, [])
+
+  const fetchLectures = async () => {
+    let { data: lectures, error } = await supabase.from('Lectures').select('*').order('id', true);
+    if (error) console.log('error', error);    
+    else setLectures(lectures);
+  }
+
+  return (
+    <div>
+      <h1>LECTURES</h1>
+      <ul>
+          {lectures.map((lecture) => <li key={lecture.id}>{lecture.title}</li>)}
+      </ul>
+    </div>
+  );
 }
