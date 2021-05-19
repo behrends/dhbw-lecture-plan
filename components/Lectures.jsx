c2b1d805b52d187088b7c14ad5d5d590d0bf0317
Erik Behrends <behrends@users.noreply.github.com>
2021-05-19 14:16:29 +0200

    Lectures als Präsentationskomponente umgeschrieben

--- components/Lectures.jsx
+++ components/Lectures.jsx
@@ -1,27 +1,11 @@
-import { useEffect, useState } from 'react';
 import { Box, Heading, Stack, Text } from '@chakra-ui/react';
-import { supabase } from '../lib/initSupabase';
 
-export default function Lectures() {
-  const [lectures, setLectures] = useState([]);
-
-  useEffect(() => {
-    fetchLectures();
-  }, []);
-
-  const fetchLectures = async () => {
-    if (!supabase) return;
-    let { data: lectures, error } = await supabase
-      .from('Lectures')
-      .select('id, title')
-      .eq('semester', 1); // TIF20A, 3. Semester
-    if (error) console.log('error', error);
-    else setLectures(lectures);
-  };
-
+export default function Lectures({ course, semester, lectures }) {
   return (
     <Stack spacing={3}>
-      <Heading>Vorlesungen in TIF20A 3. Semester</Heading>
+      <Heading>
+        Vorlesungen in {course} {semester}.Semester
+      </Heading>
       {lectures.map((lecture) => (
         <Box
           key={lecture.id}
--- pages/playground.js
+++ pages/playground.js
@@ -1,5 +1,24 @@
+import { useEffect, useState } from 'react';
+import { supabase } from '../lib/initSupabase';
 import Lectures from '../components/Lectures';
 
 export default function Playground() {
-  return <Lectures />;
+  const [lectures, setLectures] = useState([]);
+
+  useEffect(() => {
+    fetchLectures();
+  }, []);
+
+  const fetchLectures = async () => {
+    if (!supabase) return;
+    let { data: lectures, error } = await supabase
+      .from('Lectures')
+      .select('id, title')
+      .eq('semester', 1); // TIF20A, 3. Semester
+    if (error) console.log('error', error);
+    else setLectures(lectures);
+  };
+  return (
+    <Lectures course="TIF20A" semester={3} lectures={lectures} />
+  );
 }
