a9df163934633ed91825d2ff7d5a6202a64b9c2c
Erik Behrends <behrends@users.noreply.github.com>
2021-06-14 12:59:12 +0200

    Card mit einfacher Story

---
+++ components/card/Card.stories.js
@@ -0,0 +1,23 @@
+import { withDesign } from 'storybook-addon-designs';
+import Card from './Card';
+
+export default {
+  title: 'Components/Card',
+  component: Card,
+  decorators: [withDesign],
+};
+
+const Template = (args) => <Card {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  title: 'Meine Karte',
+};
+
+Default.parameters = {
+  design: {
+    type: 'figma',
+    url:
+      'https://www.figma.com/file/A4gYFKN6hZwnNqLo3M5NMr/DHBW-Lecture-Plan?node-id=3%3A2',
+  },
+};
