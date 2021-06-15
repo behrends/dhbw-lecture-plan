6ad886c45e1b80b56bae32170b69b6753e15d88b
Erik Behrends <behrends@users.noreply.github.com>
2021-06-15 16:13:20 +0200

    Home vereinfacht

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import Layout from '../components/Layout';
-import '../styles/globals.css'; // TODO: brauchen wir dies noch?
 
 function MyApp({ Component, pageProps }) {
   return (
