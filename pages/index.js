35e8618217de4898baa997718008f21dfc328727
Erik Behrends <behrends@users.noreply.github.com>
2021-06-16 11:21:32 +0200

    Home: Größere Schrift

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,20 @@
-import { Box, Heading } from '@chakra-ui/react';
+import { Box, Heading, Text } from '@chakra-ui/react';
 import PageHead from '../components/PageHead';
 
 export default function Home() {
   return (
     <Box>
       <PageHead title="DHBW Studienplan" />
-      <Heading>DHBW Studienplan</Heading>
+      <Heading as="h1" size="3xl">
+        DHBW Studienplan
+      </Heading>
+      <Heading as="h2" size="lg">
+        Digitalisierung des Studienbetriebs
+      </Heading>
+      <Text>
+        Ein Beispiel-Projekt u.a. für die Lehrveranstaltungen
+        Web-Engineering und Anwendungsprojekt.
+      </Text>
     </Box>
   );
 }
